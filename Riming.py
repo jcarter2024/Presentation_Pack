1f8f880e6d07b8ab5230a611e2c7517921f5df4a
Joshua Carter <joshua.carter@postgrad.manchester.ac.uk>
2021-02-18 10:17:04 +0000

    Riming sim typo

--- Riming.py
+++ Riming.py
@@ -1,27 +1,19 @@
-from vpython import *
-#GlowScript 3.0 VPython
-win = 500
+GlowScript 3.0 VPython
+win = 1000
 
 # Typical values
 L = 1 # container is a cube L on a side
 gray = color.gray(0.7) # color of edges of container
 mass = 4E-3/6E23 # helium mass
-Ratom = 0.03 # wildly exaggerated size of helium atom
-k = 1.4E-23 # Boltzmann constant
 T = 300 # around room temperature
 dt = 1E-5
-Ndrops = 200
-
+Ndrops = 800
+n=0
 animation = canvas( width=win, height=win, align='left')
 animation.range = L
 animation.title = 'The riming process'
-s = """  Theoretical and averaged speed distributions (meters/sec).
-  Initially all atoms have the same speed, but collisions
-  change the speeds of the colliding atoms. One of the atoms is
-  marked and leaves a trail so you can follow its path."""
-animation.caption = s
 gray = color.gray(0.7) # color of edges of container
-Ratom = 0.003 # wildly exaggerated size of helium atom
+Ratom = 0.005 # wildly exaggerated size of helium atom
        
 d = L/2+Ratom
 r = 0.005 #box radius
@@ -38,15 +30,15 @@ vert1.append([vector(-d,-d,-d), vector(-d,d,-d)])
 vert2.append([vector(-d,-d,d), vector(-d,d,d)])
 vert3.append([vector(d,-d,d), vector(d,d,d)])
 vert4.append([vector(d,-d,-d), vector(d,d,-d)])
-ice_length = 0.5/2
-ice_width = 0.5/2
-ice_height = 0.1/2
-ice=ellipsoid(pos=vector(0,0,0),length=ice_length, height=ice_height, width=ice_width, color=color.cyan, make_trail=True, retain=10, trail_radius=0.1, opacity=0.2)
+base_length = 0.5/2.5
+base_width = 0.5/2.5
+base_height = 0.1/2.5
+ice=ellipsoid(pos=vector(0,0,0),length=base_length, height=base_height, width=base_width, color=color.cyan, make_trail=True, retain=10, trail_radius=0.1, opacity=0.5)
 
 Atoms = []
 p = [] #momentum 
 apos = [] #position
-pavg=2 # average kinetic energy p**2/(2mass) = (3/2)kT
+pavg=20 # average kinetic energy p**2/(2mass) = (3/2)kT
     
 for i in range(Ndrops): #initialisation
     x = L*random()-L/2
@@ -63,11 +55,30 @@ for i in range(Ndrops): #initialisation
     py = pavg
     pz = 0
     p.append(vector(px,py,pz))
-
-while True:
-    rate(300)
     
+ratenumber=300
+old_count=0
+new_count=0
+true_number = 0
+while True:
+    rate(ratenumber) #operations per second?
+#    old_count += new_count
+    #Ndrops+=new_count
+    new_count=0
     for i in range(Ndrops): 
+        s = "  absorbed droplets = "+str(new_count)+" with height "+str(ice.height)+"total drops "+str(Ndrops)
+        animation.caption = s
+        loc = apos[i]
+        if ((loc.y)**2/((ice.height*0.5)**2)) + ((loc.x**2)/((ice.length*0.5)**2)) + ((loc.z**2)/((ice.width*0.5)**2))< 1:
+            apos[i].y = apos[i].y 
+            apos[i].x = apos[i].x
+            apos[i].z = apos[i].z
+            new_count+=1
+            if ice.height<ice.length:
+                ice.length=base_length+(new_count*Ratom/20)
+                ice.height=base_height+(new_count*Ratom/10)
+                
+        else:
             Atoms[i].pos = apos[i] = apos[i] + (p[i]/mass)*dt #momentum transfer determines direction, need to use upward g
     
     for i in range(Ndrops): #this accounts for the box edges, I need to account for y only 
@@ -79,12 +90,10 @@ while True:
                 apos[i].x = L*random()-L/2
                 apos[i].z = L*random()-L/2
                 
-        if loc.y < ice.pos.y+ice_length: #rule to make these drops stick to the surface of ellipse 
-            if loc.x < ice.pos.x+ice_width :
-                if loc.z < ice.pos.y+ice_height:
-                    apos[i].y = apos[i].y 
-                    apos[i].x = apos[i].x
-                    apos[i].z = apos[i].z 
+    if i  == Ndrops: #I want to add to Ndrops here so that the droplet field is constant but I need to add to apos  probs dont even need a break 
+            Ndrops+=new_count
+
                 
         
+
 
